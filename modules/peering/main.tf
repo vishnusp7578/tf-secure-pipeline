71ae1d511617460aa0f751edf41e9d10516c0184
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-26 14:12:24 +0530

    Create main.tf

---
+++ modules/peering/main.tf
@@ -0,0 +1,11 @@
+variable "name" {}
+variable "network" {}
+variable "peer_network" {}
+
+resource "google_compute_network_peering" "peering" {
+  name                 = var.name
+  network              = var.network
+  peer_network         = var.peer_network
+  export_custom_routes = true
+  import_custom_routes = true
+}
