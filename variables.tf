c926c06dd98a1889e5b4f90c6eb84d5684b1bde2
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-26 14:19:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,8 +14,6 @@ variable "cloudbuild_sa_email" {
   type        = string
 }
 
-variable "project_id" {}
-variable "region" { default = "us-central1" }
 variable "zone_a" { default = "us-central1-a" }
 variable "zone_b" { default = "us-central1-b" }
 variable "my_ip" {}
