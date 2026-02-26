2a972244cb75ba0e5a115b93b3addabe07f68489
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-26 14:11:55 +0530

    Create main.tf

---
+++ modules/nat/main.tf
@@ -0,0 +1,16 @@
+variable "network" {}
+variable "region" {}
+
+resource "google_compute_router" "router" {
+  name    = "nat-router"
+  network = var.network
+  region  = var.region
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "cloud-nat"
+  router                             = google_compute_router.router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
