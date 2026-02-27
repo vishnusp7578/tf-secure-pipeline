c2b3a8db22b74d1f217cdc49351e92c577262f20
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-27 11:26:29 +0530

    Update main.tf

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -4,7 +4,7 @@ resource "google_dns_managed_zone" "public_zone" {
 }
 
 resource "google_dns_record_set" "a_record" { 
-  name         = "www.example.com."
+  name         = "www.mytf-lab-2026.com."
   type         = "A"
   ttl          = 300
   managed_zone = google_dns_managed_zone.public_zone.name
