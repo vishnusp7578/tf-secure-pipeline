5bcabded80007b5861b3a2661bc768fc1fea8f3e
vishnusp7578 <69621545+vishnusp7578@users.noreply.github.com>
2026-02-27 11:07:12 +0530

    Create main.tf

---
+++ modules/dns/main.tf
@@ -0,0 +1,28 @@
+variable "network" {}
+variable "external_ip" {}
+
+resource "google_dns_managed_zone" "public_zone" {
+  name     = "public-zone"
+  dns_name = "example.com."
+}
+
+resource "google_dns_record_set" "a_record" {
+  name         = "www.example.com."
+  type         = "A"
+  ttl          = 300
+  managed_zone = google_dns_managed_zone.public_zone.name
+  rrdatas      = [var.external_ip]
+}
+
+resource "google_dns_managed_zone" "private_zone" {
+  name     = "private-zone"
+  dns_name = "internal.local."
+
+  visibility = "private"
+
+  private_visibility_config {
+    networks {
+      network_url = var.network
+    }
+  }
+}
